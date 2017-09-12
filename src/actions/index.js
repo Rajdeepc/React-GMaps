c293283edc39d63be2fbeb7ed63b3c1d11315aeb
Rajdeepc <rajrock38@gmail.com>
2017-09-12 17:18:19 +0530

    First Commit

---
+++ src/actions/index.js
@@ -0,0 +1,27 @@
+import Firebase from 'firebase';
+
+
+// const Posts = new Firebase('https://fbredux.firebaseio.com/');
+
+// export function fetchPosts() {
+//     return dispatch => {
+//         Posts.on('value', snapshot => {
+//             dispatch({
+//                 type: FETCH_POSTS,
+//                 payload: snapshot.val()
+//             });
+//         });
+//     }
+// }
+
+
+
+// export function deletePosts() {
+//     return dispatch => 
+//         Posts.child(key).remove();
+// }
+
+
+// export function createPost(post) {
+//     return dispatch => Posts.push(post)
+// }
